d97476f0a686115c48c8feaf4dcddcf41456816a
MD Hussain <>
2021-03-13 00:01:32 +0530

    Media queries

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 window.addEventListener("scroll", () => {
-  let main_header = document.getElementById("main-header");
-  let about_text = document.getElementById("about_text");
-  main_header.classList.toggle("scrolling-active", window.scrollY > 0);
-  about_text.classList.toggle("text-focus-in", window.scrollY > 694);
+	let main_header = document.getElementById("main-header");
+	let about_text = document.getElementById("about_text");
+	main_header.classList.toggle("scrolling-active", window.scrollY > 0);
+	about_text.classList.toggle("text-focus-in", window.scrollY > 694);
 });
 
 const navigation = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".collapse");
-  const main_header = document.querySelector("#main-header");
+	const burger = document.querySelector(".burger");
+	const nav = document.querySelector(".collapse");
+	const main_header = document.querySelector("#main-header");
 
-  burger.addEventListener("click", () => {
-    main_header.classList.toggle("nav-background");
-    nav.classList.toggle("burger-active");
-    burger.classList.toggle("toggle");
-  });
+	burger.addEventListener("click", () => {
+		main_header.classList.toggle("nav-background");
+		nav.classList.toggle("burger-active");
+		burger.classList.toggle("toggle");
+	});
 };
 navigation();
